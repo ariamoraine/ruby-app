5ab9c299038ae4939b7b2ffb607c75a72df1ee29
Railsbridge Student <unconfigured-student@railsbridge.local>
2017-02-18 19:25:34 +0000

    added routes

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,7 +1,36 @@
 class JobsController < ApplicationController
   def index
+    @jobs = Job.all
   end
+
   def new
     @job = Job.new
+  end
+
+  def create
+    Job.create(job_params)
+    redirect_to jobs_url
+  end
+
+  def edit
+    @job = Job.find(params[:id])
+  end
+
+  def update
+    j = Job.find(params[:id])
+    j.update(job_params)
+    redirect_to jobs_url
+  end
+
+  def destroy
+    j = Job.find(params[:id])
+    j.destroy
+    redirect_to jobs_url
+  end
+
+  private
+
+  def job_params
+    params.require(:job).permit(:title, :description)
   end
 end

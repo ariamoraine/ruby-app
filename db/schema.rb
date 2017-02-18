5ca3c3e5640ce82c2f99ae96cf8c43a764963191
Railsbridge Student <unconfigured-student@railsbridge.local>
2017-02-18 21:06:01 +0000

    adding some css

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170218162606) do
+ActiveRecord::Schema.define(version: 20170218201652) do
 
   create_table "jobs", force: :cascade do |t|
     t.string   "title"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20170218162606) do
     t.string   "company"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "salary"
   end
 
 end
